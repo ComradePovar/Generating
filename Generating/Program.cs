01fdb986cc9589187b3c7afb90ac2147a458736a
Tokarev Maxim <nepfion96@gmail.com>
2017-04-07 01:09:03 +0300

    Noob mesh, diamond-square alg

--- Generating/Program.cs
+++ Generating/Program.cs
@@ -12,21 +12,31 @@ namespace Generating
     {
         private Texture2D texture;
         private View view;
-        private int W = 1000;
-        private int H = 1000;
+        private int W = 65;
+        private int H = 65;
+        TerrainGenerator terrainGenerator;
+        Vector3[] vertBuffer;
+        int VBO;
+
         public Game()
             : base(800, 600, GraphicsMode.Default, "OpenTK")
         {
             VSync = VSyncMode.On;
-            GL.Enable(EnableCap.Texture2D);
-            view = new View(Vector3.Zero, 1.5f);
+            //GL.Enable(EnableCap.Texture2D);
+            //view = new View(Vector3.Zero, 1.5f);
+            terrainGenerator = new TerrainGenerator();
         }
 
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
-
-            texture = AssetsLoader.LoadTexture("land.png");
+            
+            if (!isCalculated)
+            {
+                terrainGenerator.GenerateHeightMap(W, H, 5, 5, 5, 5, 10);
+                isCalculated = true;
+            }
+            //texture = AssetsLoader.LoadTexture("land.png");
             GL.ClearColor(0.1f, 0.2f, 0.5f, 0.0f);
             GL.Enable(EnableCap.DepthTest);
         }
@@ -35,49 +45,58 @@ namespace Generating
         {
             base.OnResize(e);
 
-            //GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+            GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
 
-            //Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4f, Width / (float)Height, 1.0f, 200.0f);
-            //GL.MatrixMode(MatrixMode.Projection);
-            //GL.LoadMatrix(ref projection);
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4f, Width / (float)Height, 1.0f, 2000.0f);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
         }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             base.OnUpdateFrame(e);
 
-            view.Update();
+            //view.Update();
         }
-
+        private bool isCalculated = false;
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
            
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-            SpriteHandler.Begin(Width, Height);
-            view.Transform();
+            Matrix4 modelView = Matrix4.LookAt(new Vector3(32f, 50, -40f), new Vector3(32f, 0, 32f), Vector3.UnitY);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref modelView);
+            //SpriteHandler.Begin(Width, Height);
+            //view.Transform();
 
-            Random rand = new Random(1);
-            for (int i = 0; i < 20; i++)
-                for (int j = 0; j < 20; j++)
+
+            GL.EnableClientState(ArrayCap.VertexArray);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+            GL.VertexPointer(3, VertexPointerType.Float, Vector3.SizeInBytes, 0);
+
+            GL.Color3(Color.Red);
+            //GL.DrawElements(BeginMode.TriangleStrip, W*H + (W-1)*(H-2), DrawElementsType.UnsignedInt, ())
+            for (int i = 0; i < W; i++)
+            {
+                GL.Begin(BeginMode.LineStrip);
+                for (int j = 0; j < H; j++)
                 {
-                    int c = rand.Next(1, 4);
-                    switch (c)
-                    {
-                        case 1:
-                            SpriteHandler.Draw(texture, Vector2.Zero, new Vector2(1f, 1f), Color.LightGray, new Vector2(10f * j, 10f * i));
-                            break;
-                        case 2:
-                            SpriteHandler.Draw(texture, Vector2.Zero, new Vector2(1f, 1f), Color.Gray, new Vector2(10f * j, 10f * i));
-                            break;
-                        case 3:
-                            SpriteHandler.Draw(texture, Vector2.Zero, new Vector2(1f, 1f), Color.DarkGray, new Vector2(10f * j, 10f * i));
-                            break;
-
-                    }
+                    GL.Vertex3(i, terrainGenerator.HeightMap[i, j], j);
                 }
-
+                GL.End();
+            }
+            for (int i = 0; i < H; i++)
+            {
+                GL.Begin(BeginMode.LineStrip);
+                for (int j = 0; j < W; j++)
+                {
+                    GL.Vertex3(j, terrainGenerator.HeightMap[j, i], i);
+                }
+                GL.End();
+            }
+            GL.Flush();
             SwapBuffers();
         }
         static void Main()
---
+++ Generating/TerrainGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace Generating
+{
+    public class TerrainGenerator
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public float Roughness { get; set; }
+        public float Displacement { get; set; }
+        public float[,] HeightMap { get; private set; }
+
+        private static Random rand = new Random();
+
+        public void GenerateHeightMap(int width, int height, float topLeft, float bottomLeft, float bottomRight, float topRight, float roughness)
+        {
+            Width = width;
+            Height = height;
+            Roughness = roughness;
+
+            HeightMap = new float[Width, Height];
+
+            DiamondSquareAlgorithm(topLeft, bottomLeft, bottomRight, topRight);
+        }
+
+        private void DiamondSquareAlgorithm(float topLeft, float bottomLeft, float bottomRight, float topRight)
+        {
+            int maxIndex = Width - 1;
+            
+            //Init corners
+            HeightMap[0, 0] = topLeft;
+            HeightMap[maxIndex, 0] = bottomLeft;
+            HeightMap[maxIndex, maxIndex] = bottomRight;
+            HeightMap[0, maxIndex] = topRight;
+
+            for (int squareSize = maxIndex; squareSize >= 2; squareSize /= 2, Roughness /= 2)
+            {
+                int stepSize = squareSize / 2;
+
+                //Squares
+                for (int x = 0; x < maxIndex; x += squareSize)
+                {
+                    for (int y = 0; y < maxIndex; y += squareSize)
+                    {
+                        HeightMap[x + stepSize, y + stepSize] = GetRandomFloat(-Roughness, Roughness) +
+                                                                (HeightMap[x, y] + HeightMap[x + squareSize, y] + HeightMap[x, y + squareSize] +
+                                                                 HeightMap[x + squareSize, y + squareSize]) / 4;
+                    }
+                }
+
+                //Diamonds
+                for (int x = 0; x < maxIndex; x += stepSize)
+                {
+                    for (int y = (x + stepSize) % squareSize; y < maxIndex; y += squareSize)
+                    {
+                        HeightMap[x, y] = GetRandomFloat(-Roughness, Roughness) +
+                                          (HeightMap[(x - stepSize + Width) % Width, y] + HeightMap[(x + stepSize) % Width, y] +
+                                           HeightMap[x, (y + stepSize) % Width] + HeightMap[x, (y - stepSize + Width) % Width]) / 4;
+                        if (x == 0)
+                            HeightMap[maxIndex, y] = HeightMap[x, y];
+                        if (y == 0)
+                            HeightMap[x, maxIndex] = HeightMap[x, y];
+                    }
+                }
+            }
+
+        }
+
+        private float GetRandomFloat(float min, float max)
+        {
+            return (float)rand.NextDouble() * (max - min) + min;
+        }
+    }
+}
