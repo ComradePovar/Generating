b176c4f122af1225cfc45fdb3c8b4afa37c5be1a
Tokarev Maxim <nepfion96@gmail.com>
2017-04-09 20:31:20 +0300

    Shaders

--- Generating/Program.cs
+++ Generating/Program.cs
@@ -3,8 +3,10 @@ using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
+using System.Drawing;
+using System.Drawing.Imaging;
 
-namespace StarterKit
+namespace Generating
 {
     class Game : GameWindow
     {
@@ -14,32 +16,20 @@ namespace StarterKit
         private static int N = 100000;
 
         public Game()
-            : base(800, 600, GraphicsMode.Default, "OpenTK")
+            : base(800, 800, GraphicsMode.Default, "OpenTK")
         {
             VSync = VSyncMode.On;
         }
 
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-            brokenLine = new float[2, N + 1];
-            brokenLine[1, 0] = -3.5f;
-            brokenLine[1, N] = -3.5f;
-            for (int i = 0; i < brokenLine.GetLength(1); i++)
-            {
-                brokenLine[0, i] = -5f + i * (float)(ScreenW) / N;
-            }
-            GL.ClearColor(0.1f, 0.2f, 0.5f, 0.0f);
-            GL.Enable(EnableCap.DepthTest);
-        }
 
+        Matrix4 projection;
         protected override void OnResize(EventArgs e)
         {
             base.OnResize(e);
 
             GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
-            
-            Matrix4 projection = Matrix4.CreateOrthographic(ScreenW, ScreenH, -1, 1);
+
+            projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)Width / Height, 1, 1000);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadMatrix(ref projection);
         }
@@ -51,90 +41,159 @@ namespace StarterKit
             if (Keyboard[Key.Escape])
                 Exit();
         }
+        int textureId = 0;
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            brokenLine = new float[2, N + 1];
+            brokenLine[1, 0] = -3.5f;
+            brokenLine[1, N] = -3.5f;
+            for (int i = 0; i < brokenLine.GetLength(1); i++)
+            {
+                brokenLine[0, i] = -5f + i * (float)(ScreenW) / N;
+            }
+            GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+            textureId = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, textureId);
 
+            Bitmap bmp = new Bitmap("Assets/land.jpg");
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Rgba, PixelType.UnsignedByte, data.Scan0);
+            bmp.UnlockBits(data);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+            GL.ClearColor(0.1f, 0.2f, 0.5f, 0.0f);
+            GL.Enable(EnableCap.DepthTest);
+            shader = new ShaderProgram();
+            vao = new VAO();
+        }
+        ShaderProgram shader;
+        VAO vao;
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 4f, 0),
+            new Vector3(4f, 2f, 0),
+            new Vector3(8f, 3f, 0),
+            new Vector3(12, 1, 0),
+            new Vector3(0, 3, 4),
+            new Vector3(4f, 5f, 4),
+            new Vector3(8f, 8f, 4),
+            new Vector3(12f, 2f, 4),
+            new Vector3(0, 7, 8),
+            new Vector3(4f, 10f, 8),
+            new Vector3(8f, 12f, 8),
+            new Vector3(12, 6, 8),
+            new Vector3(0, 4, 12),
+            new Vector3(4f, 6f, 12),
+            new Vector3(8f, 8f, 12),
+            new Vector3(12, 3, 12),
+
+        };
+        uint[] indices = new uint[]
+        {
+            0, 4, 1, 5, 2, 6, 3, 7, 16,
+            4, 8, 5, 9, 6, 10, 7, 11, 16,
+            8, 12, 9, 13, 10, 14, 11, 15
+        };
+        Vector3[] colors = new Vector3[] { new Vector3(0, 0, 1f), new Vector3(0, 1f, 0), new Vector3(1f, 0, 0), new Vector3(0, 1, 0f), new Vector3(0, 0, 1f) };
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-            Matrix4 modelview = Matrix4.LookAt(Vector3.Zero, -Vector3.UnitZ, Vector3.UnitY);
-            GL.MatrixMode(MatrixMode.Modelview);
-            GL.LoadMatrix(ref modelview);
+            //GL.Enable(EnableCap.Texture2D);
+            //GL.TexEnv(TextureEnvTarget.TextureEnv, TextureEnvParameter.TextureEnvMode, (int)TextureEnvMode.Replace);
+            DrawTriangle();
 
+           // DrawQuad();
 
-            DrawBrokenDiagonal();
-
+            //Matrix4 modelview = Matrix4.LookAt(Vector3.Zero, -Vector3.UnitZ, Vector3.UnitY);
+            //GL.MatrixMode(MatrixMode.Modelview);
+            //GL.LoadMatrix(ref modelview);
+            
             SwapBuffers();
         }
-
-        private bool isCalculated = false;
-        private void DrawBrokenDiagonal()
+        float angle1 = 0;
+        float angle2 = 0;
+        void DrawQuad()
         {
-            if (!isCalculated)
-            {
-                BrokenDiagonal(0, N, -3.5f, -3.5f);
-                isCalculated = true;
-            }
-            GL.Color3(System.Drawing.Color.Red);
-            GL.Begin(BeginMode.Polygon);
+            GL.Begin(BeginMode.Quads);
 
-            for (int i = 0; i < brokenLine.GetLength(1); i++)
-            {
-                Vector3d vec = new Vector3d(brokenLine[0, i], brokenLine[1, i], 0);
-                GL.Vertex3(vec);
-            }
-            GL.Vertex3(5.0f, -5f, 0);
-            GL.Vertex3(-5.0f, -5f, 0);
-            GL.End();
-        }
 
-        private float roughness = 0.7f;
-        private Random rand = new Random();
-        private void BrokenDiagonal(int left, int right, float valL, float valR)
-        {
-            if (right - left == 1)
-                return;
-            int midPoint = (right + left) / 2;
-            float lineLength = brokenLine[0, right] - brokenLine[0, left];
-            brokenLine[1, midPoint] = (valR + valL) / 2 + Math.Abs(Random(lineLength * -roughness, lineLength * roughness));
-            BrokenDiagonal(left, midPoint, brokenLine[1, left], brokenLine[1, midPoint]);
-            BrokenDiagonal(midPoint, right, brokenLine[1, midPoint], brokenLine[1, right]);
-        }
-        private float Random(float low, float high)
-        {
-            return (float)rand.NextDouble() * (high - low) + low;
-        }
-
-        void DrawTriangle()
-        {
-            GL.Begin(BeginMode.Triangles);
-
-            //GL.Color3(1.0f, 1.0f, 0.0f);
-            GL.Color3(System.Drawing.Color.Red);
-            GL.Vertex3(-1.0f, -1.0f, 0f);
+            //GL.Color3(System.Drawing.Color.Red);
+            //GL.TexCoord2(0, 1);
+            GL.Vertex3(0.0f, -1.0f, 0f);
             //GL.Color3(1.0f, 0.0f, 0.0f);
-            GL.Color3(System.Drawing.Color.Green);
-            GL.Vertex3(1.0f, -1.0f, 0f);
+            //GL.Color3(System.Drawing.Color.Green);
+            //GL.TexCoord2(1, 1);
+            GL.Vertex3(2.0f, -1.0f, 0f);
             //GL.Color3(0.2f, 0.9f, 1.0f);
-            GL.Color3(System.Drawing.Color.Blue);
+            //GL.Color3(System.Drawing.Color.Blue);
+            //GL.TexCoord2(1, 0);
+            GL.Vertex3(2.0f, 1.0f, 0f);
+            //GL.Color3(Color.Yellow);
+            //GL.TexCoord2(0, 0);
             GL.Vertex3(0.0f, 1.0f, 0f);
 
+
             GL.End();
         }
-        void DrawRectangle()
+        void DrawTriangle()
         {
-            GL.Begin(BeginMode.Polygon);
+            GL.Enable(EnableCap.VertexArray);
+            GL.Enable(EnableCap.IndexArray);
+            GL.Enable(EnableCap.PrimitiveRestart);
+            GL.PrimitiveRestartIndex(16);
 
-            GL.Color3(System.Drawing.Color.Black);
-            GL.Vertex3(-1.0f, -1.0f, 0f);
-            GL.Vertex3(-1.0f, 1.0f, 0f);
-            GL.Color3(System.Drawing.Color.DarkRed);
-            GL.Vertex3(1.0f, 1.0f, 0f);
-            GL.Vertex3(1.0f, -1.0f, 0f);
+            vao.BindVerticesBuffer(vertices);
+            vao.BindIndicesBuffer(indices);
 
-            GL.End();
+            GL.BindVertexArray(vao.ID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vao.VerticesBuffer);
+            GL.VertexPointer(3, VertexPointerType.Float, Vector3.SizeInBytes, IntPtr.Zero);
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, vao.IndicesBuffer);
+            shader.UseProgram();
+            //GL.DrawElements(BeginMode.Triangles, indices.Length, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            //GL.DrawArrays(BeginMode.Triangles, 0, indices.Length);
 
+            int modelView = GL.GetUniformLocation(shader.ID, "modelViewMatrix");
+            int projection = GL.GetUniformLocation(shader.ID, "projectionMatrix");
+            GL.UniformMatrix4(projection, false, ref this.projection);
+
+            
+
+            
+            //
+            //GL.DrawElements(BeginMode.Triangles, indices.Length, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            Matrix4 modelview = Matrix4.LookAt(new Vector3(8, 40, -8), new Vector3(3, 0, 3), new Vector3(0.0f, 1.0f, 0.0f));
+            Matrix4 current = modelview;
+            GL.UniformMatrix4(modelView, false, ref current);
+            //GL.MatrixMode(MatrixMode.Modelview);
+            //GL.LoadMatrix(ref modelview);
+            GL.DrawElements(BeginMode.TriangleStrip, indices.Length, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            //GL.Begin(BeginMode.Triangles);
+
+
+            //GL.Color3(System.Drawing.Color.Red);
+            //GL.TexCoord2(0, 1);
+            //GL.Vertex3(-2.0f, -1.0f, 0f);
+            ////GL.Color3(1.0f, 0.0f, 0.0f);
+            //GL.Color3(System.Drawing.Color.Green);
+            //GL.TexCoord2(1, 1);
+            //GL.Vertex3(0.0f, -1.0f, 0f);
+            ////GL.Color3(0.2f, 0.9f, 1.0f);
+            //GL.Color3(System.Drawing.Color.Blue);
+            //GL.TexCoord2(0.5, 0);
+            //GL.Vertex3(-1.0f, 1.0f, 0f);
+
+            //GL.End();
         }
         [STAThread]
         static void Main()
---
+++ Generating/ShaderProgram.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Generating
+{
+    class ShaderProgram
+    {
+        public int ID;
+        public int VertexShader;
+        public int FragmentShader;
+
+        public ShaderProgram()
+        {
+            ID = GL.CreateProgram();
+            AddShader("vertex.txt", "fragment.txt");
+
+            GL.AttachShader(ID, VertexShader);
+            GL.AttachShader(ID, FragmentShader);
+            GL.LinkProgram(ID);
+        }
+
+        public void AddShader(string vertexShaderPath, string fragmentShaderPath)
+        {
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            string sourceVertex = System.IO.File.ReadAllText(vertexShaderPath);
+            string sourceFragment = System.IO.File.ReadAllText(fragmentShaderPath);
+            GL.ShaderSource(VertexShader, sourceVertex);
+            GL.ShaderSource(FragmentShader, sourceFragment);
+
+            GL.CompileShader(VertexShader);
+            GL.CompileShader(FragmentShader);
+            int compileStatusVertex = 0;
+            int compileStatusFragment = 0;
+            GL.GetShader(VertexShader, ShaderParameter.CompileStatus, out compileStatusVertex);
+            GL.GetShader(FragmentShader, ShaderParameter.CompileStatus, out compileStatusFragment);
+            if (compileStatusFragment != 1 && compileStatusVertex != 1)
+            {
+                Console.WriteLine(GL.GetShaderInfoLog(VertexShader));
+                Console.WriteLine(GL.GetShaderInfoLog(FragmentShader));
+                throw new Exception();
+            }
+        }
+
+        public void UseProgram()
+        {
+            GL.UseProgram(ID);
+        }
+    }
+    class Shader
+    {
+
+    }
+}
