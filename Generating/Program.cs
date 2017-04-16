46af029ba404bc7616d1c452fcc08053efcf07ef
Tokarev Maxim <nepfion96@gmail.com>
2017-04-16 23:18:54 +0300

    123

--- Generating/Program.cs
+++ Generating/Program.cs
@@ -40,6 +40,14 @@ namespace Generating
 
             if (Keyboard[Key.Escape])
                 Exit();
+            if (Keyboard[Key.A])
+                eye.X += 0.1f;
+            if (Keyboard[Key.D])
+                eye.X -= 0.1f;
+            if (Keyboard[Key.W])
+                eye.Y += 0.1f;
+            if (Keyboard[Key.S])
+                eye.Y -= 0.1f;
         }
         int textureId = 0;
         protected override void OnLoad(EventArgs e)
@@ -71,33 +79,7 @@ namespace Generating
         }
         ShaderProgram shader;
         VAO vao;
-        Vector3[] vertices = new Vector3[]
-        {
-            new Vector3(0f, 4f, 0),
-            new Vector3(4f, 2f, 0),
-            new Vector3(8f, 3f, 0),
-            new Vector3(12, 1, 0),
-            new Vector3(0, 3, 4),
-            new Vector3(4f, 5f, 4),
-            new Vector3(8f, 8f, 4),
-            new Vector3(12f, 2f, 4),
-            new Vector3(0, 7, 8),
-            new Vector3(4f, 10f, 8),
-            new Vector3(8f, 12f, 8),
-            new Vector3(12, 6, 8),
-            new Vector3(0, 4, 12),
-            new Vector3(4f, 6f, 12),
-            new Vector3(8f, 8f, 12),
-            new Vector3(12, 3, 12),
-
-        };
-        uint[] indices = new uint[]
-        {
-            0, 4, 1, 5, 2, 6, 3, 7, 16,
-            4, 8, 5, 9, 6, 10, 7, 11, 16,
-            8, 12, 9, 13, 10, 14, 11, 15
-        };
-        Vector3[] colors = new Vector3[] { new Vector3(0, 0, 1f), new Vector3(0, 1f, 0), new Vector3(1f, 0, 0), new Vector3(0, 1, 0f), new Vector3(0, 0, 1f) };
+        Vector3 eye = new Vector3(0f, 0f, 0f);
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
@@ -141,43 +123,114 @@ namespace Generating
 
             GL.End();
         }
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0.5f, -0.5f, -0.5f),
+            new Vector3(0.5f, 0.5f, -0.5f),
+            new Vector3(-0.5f, 0.5f, -0.5f),
+            new Vector3(-0.5f, -0.5f, -0.5f),
+            new Vector3(0.5f, -0.5f, 0.5f),
+            new Vector3(0.5f, 0.5f, 0.5f),
+            new Vector3(-0.5f, 0.5f, 0.5f),
+            new Vector3(-0.5f, -0.5f, 0.5f),
+
+        };
+        uint[] indices = new uint[]
+        {
+            0, 1, 2, 3, 3, 2, 6, 7, 4, 5, 6, 7, 4, 5, 1, 0, 1, 2, 6, 5
+        };
+        Vector3[] colors = new Vector3[] {
+            new Vector3(0, 0.2f, 0),
+            new Vector3(0, 0.2f, 0),
+            new Vector3(0, 0.2f, 0),
+            new Vector3(0f, 0.2f, 0f),
+            new Vector3(0, 0.2f, 0),
+            new Vector3(0, 0.2f, 0),
+            new Vector3(0, 0.2f, 0),
+            new Vector3(0f, 0.2f, 0f),
+        };
+        Vector3[] normals = new Vector3[] {
+            new Vector3(1f, 0f, 0f),
+            new Vector3(1f, 0f, 0f),
+            new Vector3(0f, 0f, 1f),
+            new Vector3(0f, 0f, 1f),
+            new Vector3(0f, 0f, 1f),
+            new Vector3(0f, 0f, 1f),
+            new Vector3(0f, 0f, 1f),
+            new Vector3(0f, 0f, 1f),
+        };
+        float k;
         void DrawTriangle()
         {
             GL.Enable(EnableCap.VertexArray);
             GL.Enable(EnableCap.IndexArray);
+            GL.Enable(EnableCap.NormalArray);
+            GL.Enable(EnableCap.ColorArray);
             GL.Enable(EnableCap.PrimitiveRestart);
             GL.PrimitiveRestartIndex(16);
 
             vao.BindVerticesBuffer(vertices);
             vao.BindIndicesBuffer(indices);
+            vao.BindColorsBuffer(colors);
+            vao.BindNormalsBuffer(normals);
 
             GL.BindVertexArray(vao.ID);
             GL.BindBuffer(BufferTarget.ArrayBuffer, vao.VerticesBuffer);
-            GL.VertexPointer(3, VertexPointerType.Float, Vector3.SizeInBytes, IntPtr.Zero);
             GL.EnableVertexAttribArray(0);
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vao.ColorsBuffer);
+            GL.EnableVertexAttribArray(1);
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 0, 0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vao.NormalsBuffer);
+            GL.NormalPointer(NormalPointerType.Float, 0, 0);
+            GL.EnableVertexAttribArray(2);
+            GL.VertexAttribPointer(2, 3, VertexAttribPointerType.Float, false, 0, 0);
             
             GL.BindBuffer(BufferTarget.ElementArrayBuffer, vao.IndicesBuffer);
             shader.UseProgram();
             //GL.DrawElements(BeginMode.Triangles, indices.Length, DrawElementsType.UnsignedInt, IntPtr.Zero);
             //GL.DrawArrays(BeginMode.Triangles, 0, indices.Length);
 
-            int modelView = GL.GetUniformLocation(shader.ID, "modelViewMatrix");
+            //Matrix4 model = Matrix4.CreateTranslation(0, 0, 1);
+            Matrix4 model = Matrix4.CreateRotationY(MathHelper.DegreesToRadians(k));
+            // eye = new Vector3(2f, 1f, 4f);
+            k += 0.01f;
+            Matrix4 view = Matrix4.LookAt(eye, new Vector3(0, 0, 0f), new Vector3(0.0f, 1.0f, 0.0f));
+            Matrix4 normal = Matrix4.Transpose(Matrix4.Invert(view * model));
+            Vector3 direction = new Vector3(-10f, 5f, -10f);
+            direction.Normalize();
+            direction = -direction;
+            Vector3 color = new Vector3(1f, 1f, 1f);
+
+            int modelMatrix = GL.GetUniformLocation(shader.ID, "modelMatrix");
+            GL.UniformMatrix4(modelMatrix, false, ref model);
+            int viewMatrix = GL.GetUniformLocation(shader.ID, "viewMatrix");
             int projection = GL.GetUniformLocation(shader.ID, "projectionMatrix");
+            int normalMatrix = GL.GetUniformLocation(shader.ID, "normalMatrix");
+            int lDirection = GL.GetUniformLocation(shader.ID, "lDirection");
+            int lColor = GL.GetUniformLocation(shader.ID, "lColor");
+            int ambientIntensity = GL.GetUniformLocation(shader.ID, "ambientIntensity");
+            int specularPower = GL.GetUniformLocation(shader.ID, "specularPower");
+            int specularIntensity = GL.GetUniformLocation(shader.ID, "specularIntensity");
+            int eyePos = GL.GetUniformLocation(shader.ID, "eyePos");
+
             GL.UniformMatrix4(projection, false, ref this.projection);
-
-            
-
-            
-            //
-            //GL.DrawElements(BeginMode.Triangles, indices.Length, DrawElementsType.UnsignedInt, IntPtr.Zero);
-            Matrix4 modelview = Matrix4.LookAt(new Vector3(8, 40, -8), new Vector3(3, 0, 3), new Vector3(0.0f, 1.0f, 0.0f));
-            Matrix4 current = modelview;
-            GL.UniformMatrix4(modelView, false, ref current);
+            GL.Uniform3(lDirection, ref direction);
+            GL.Uniform1(ambientIntensity, 1f);
+            GL.Uniform3(lColor, ref color);
+            GL.Uniform1(specularPower, 128);
+            GL.Uniform1(specularIntensity, -100);
+            GL.Uniform3(eyePos, ref eye);
+            GL.UniformMatrix4(viewMatrix, false, ref view);
+            GL.UniformMatrix4(modelMatrix, false, ref model);
+            GL.UniformMatrix4(normalMatrix, false, ref normal);
+            //GL.UniformMatrix4(viewMatrix, false, ref model);
             //GL.MatrixMode(MatrixMode.Modelview);
             //GL.LoadMatrix(ref modelview);
-            GL.DrawElements(BeginMode.TriangleStrip, indices.Length, DrawElementsType.UnsignedInt, IntPtr.Zero);
-
+            GL.DrawElements(BeginMode.Quads, indices.Length, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            GL.UniformMatrix4(modelMatrix, false, ref model);
             //GL.Begin(BeginMode.Triangles);
 
 
---
+++ Generating/Terrain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generating
+{
+    class Terrain
+    {
+        public float[,] HeightMap { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public VAO VerticesData { get; set; } = new VAO();
+    }
+}
